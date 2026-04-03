aa71c1b0e221c077f3620a80e18d5349c2087320
Jonathan Muchewicz <jonathanandres.elyonny@gmail.com>
2026-04-03 19:09:41 -0300

    cambiar la referencia del servidor para escuchar en el puerto

--- src/app.js
+++ src/app.js
@@ -39,6 +39,6 @@ configureSockets(io);
 app.set('io', io); // Para acceder a io desde los controladores
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`)
 });
